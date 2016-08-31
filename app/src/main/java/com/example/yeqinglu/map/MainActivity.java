2f1aac89c798880e1ad1ddccb31e76d29fe68ec8
Yeqing <yeqing15@gmail.com>
2016-08-31 15:20:59 +0800

    update MyLocation

--- app/src/main/java/com/example/yeqinglu/map/MainActivity.java
+++ app/src/main/java/com/example/yeqinglu/map/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.yeqinglu.map;
 
+
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.location.Poi;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
@@ -10,12 +18,50 @@ import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.Marker;
 import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.PolygonOptions;
+import com.baidu.mapapi.map.Stroke;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiCitySearchOption;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     MapView mMapView = null;
     BaiduMap mBaiduMap = null;
+    PoiSearch mPoiSearch = null;
+
+    MyLocationConfiguration.LocationMode mCurrentMode;
+
+    //LocationClient类必须在主线程中声明。需要Context类型的参数。
+    //Context需要时全进程有效的context,推荐用getApplicationConext获取全进程有效的context
+    public LocationClient mLocationClient = null;
+    public BDLocationListener myListener = new MyLocationListener();
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,41 +70,258 @@ public class MainActivity extends AppCompatActivity {
         //注意该方法要再setContentView方法之前实现
         SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+
+        //定位
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener( myListener );    //注册监听函数
+
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
-
         mBaiduMap = mMapView.getMap();
 
+
         //设置地图类型
-//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //普通地图
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        //卫星地图
 //        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+        //空白地图
 //        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NONE);
 
         //设置交通，热力图
+        //打开实时交通图
         mBaiduMap.setTrafficEnabled(true);
-//        mBaiduMap.setBaiduHeatMapEnabled(true)
+        //城市热力图
+//        mBaiduMap.setBaiduHeatMapEnabled(true);
+
+//        //定义Maker坐标点
+//        LatLng point = new LatLng(39.963175, 116.400244);
+//        //构建Marker图标
+//        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.mark);
+        //构建MarkerOption，用于在地图上添加Marker
+//        OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
+//        //在地图上添加Marker，并显示
+//        mBaiduMap.addOverlay(option);
+
+
+//        //第一步，设置可拖拽：
+//        OverlayOptions options = new MarkerOptions()
+//                .position(point)  //设置marker的位置
+//                .icon(bitmap)  //设置marker图标
+//                .zIndex(9)  //设置marker所在层级
+//                .draggable(true);  //设置手势拖拽
+//        //将marker添加到地图上
+//        Marker marker = (Marker) (mBaiduMap.addOverlay(options));
+
+//        mBaiduMap.setOnMarkerDragListener(new BaiduMap.OnMarkerDragListener() {
+//            public void onMarkerDrag(Marker marker) {
+//                //拖拽中
+//            }
+//            public void onMarkerDragEnd(Marker marker) {
+//                //拖拽结束
+//            }
+//            public void onMarkerDragStart(Marker marker) {
+//                //开始拖拽
+//            }
+//        });
+
 
         //定义Maker坐标点
         LatLng point = new LatLng(39.963175, 116.400244);
-        //构建Marker图标
-        BitmapDescriptor bitmap = BitmapDescriptorFactory.fromResource(R.drawable.mark);
-        //构建MarkerOption，用于在地图上添加Marker
-        OverlayOptions option = new MarkerOptions().position(point).icon(bitmap);
-        //在地图上添加Marker，并显示
-        mBaiduMap.addOverlay(option);
+        //设置动画效果
+        BitmapDescriptor bdA = BitmapDescriptorFactory.fromResource(R.drawable.bd1);
+        BitmapDescriptor bdB = BitmapDescriptorFactory.fromResource(R.drawable.bd2);
+        BitmapDescriptor bdC = BitmapDescriptorFactory.fromResource(R.drawable.bd3);
 
-        //第一步，设置可拖拽：
-        OverlayOptions options = new MarkerOptions()
-                .position(point)  //设置marker的位置
-                .icon(bitmap)  //设置marker图标
-                .zIndex(9)  //设置marker所在层级
-                .draggable(true);  //设置手势拖拽
-        //将marker添加到地图上
-        Marker marker = (Marker) (mBaiduMap.addOverlay(options));
+        // 通过marker的icons设置一组图片，再通过period设置多少帧刷新一次图片资源
+        ArrayList<BitmapDescriptor> giflist = new ArrayList<BitmapDescriptor>();
+        giflist.add(bdA);
+        giflist.add(bdB);
+        giflist.add(bdC);
+        OverlayOptions ooD = new MarkerOptions().position(point).icons(giflist)
+                .zIndex(0).period(10);
+        Marker mMarkerD = (Marker) (mBaiduMap.addOverlay(ooD));
+
+        //设置区域覆盖
+        //定义多边形的五个顶点
+        LatLng pt1 = new LatLng(39.93923, 116.357428);
+        LatLng pt2 = new LatLng(39.91923, 116.327428);
+        LatLng pt3 = new LatLng(39.89923, 116.347428);
+        LatLng pt4 = new LatLng(39.89923, 116.367428);
+        LatLng pt5 = new LatLng(39.91923, 116.387428);
+        List<LatLng> pts = new ArrayList<LatLng>();
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+        pts.add(pt4);
+        pts.add(pt5);
+        //构建用户绘制多边形的Option对象
+        OverlayOptions polygonOption = new PolygonOptions()
+                .points(pts)
+                .stroke(new Stroke(5, 0xAA00FF00))
+                .fillColor(0xAAFFFF00);
+        //在地图上添加多边形Option，用于显示
+        mBaiduMap.addOverlay(polygonOption);
 
 
+        //设置POI
+        //第一步，创建POI检索实例
+        mPoiSearch = PoiSearch.newInstance();
+
+        // 第二步，创建POI检索监听者；
+        OnGetPoiSearchResultListener poiListener = new OnGetPoiSearchResultListener() {
+            public void onGetPoiResult(PoiResult result) {
+                //获取POI检索结果
+            }
+
+            public void onGetPoiDetailResult(PoiDetailResult result) {
+                //获取Place详情页检索结果
+            }
+
+            @Override
+            public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+
+            }
+        };
+
+        // 第三步，设置POI检索监听者；
+        mPoiSearch.setOnGetPoiSearchResultListener(poiListener);
+
+        // 第四步，发起检索请求；
+        mPoiSearch.searchInCity((new PoiCitySearchOption())
+                .city("上海").keyword("美食")
+                .pageNum(10));
+        //第五步，释放POI检索实例；
+        mPoiSearch.destroy();
+
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        //定位服务
+        initLocation();
+       /* start：启动定位SDK。 stop：关闭定位SDK。调用start之后只需要等待定位结果自动回调即可。
+        开发者定位场景如果是单次定位的场景，在收到定位结果之后直接调用stop函数即可。
+        如果stop之后仍然想进行定位，可以再次start等待定位结果回调即可。
+        如果开发者想按照自己逻辑请求定位，可以在start之后按照自己的逻辑请求locationclient.
+                requestLocation()函数，会主动触发定位SDK内部定位逻辑，等待定位回调即可。*/
+        mLocationClient.start();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
 
 
+    /*设置定位参数包括：定位模式（高精度定位模式，低功耗定位模式和仅用设备定位模式），
+    返回坐标类型，是否打开GPS，是否返回地址信息、位置语义化信息、POI信息等等。*/
+    private void initLocation(){
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy
+        );//可选，默认高精度，设置定位模式，高精度，低功耗，仅设备
+        option.setCoorType("bd09ll");//可选，默认gcj02，设置返回的定位结果坐标系
+        int span=1000;
+        option.setScanSpan(span);//可选，默认0，即仅定位一次，设置发起定位请求的间隔需要大于等于1000ms才是有效的
+        option.setIsNeedAddress(true);//可选，设置是否需要地址信息，默认不需要
+        option.setOpenGps(true);//可选，默认false,设置是否使用gps
+        option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+        option.setIsNeedLocationDescribe(true);//可选，默认false，设置是否需要位置语义化结果，可以在BDLocation.getLocationDescribe里得到，结果类似于“在北京天安门附近”
+        option.setIsNeedLocationPoiList(true);//可选，默认false，设置是否需要POI结果，可以在BDLocation.getPoiList里得到
+        option.setIgnoreKillProcess(false);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+        option.SetIgnoreCacheException(false);//可选，默认false，设置是否收集CRASH信息，默认收集
+        option.setEnableSimulateGps(false);//可选，默认false，设置是否需要过滤gps仿真结果，默认需要
+        mLocationClient.setLocOption(option);
+    }
+
+
+    /*BDLocationListener接口有1个方法需要实现：
+            1.接收异步返回的定位结果，参数是BDLocation类型参数。
+    BDLocation类，封装了定位SDK的定位结果，在BDLocationListener的
+    onReceive方法中获取。通过该类用户可以获取error code，位置的坐标，
+    精度半径等信息。*/
+    public class MyLocationListener implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mBaiduMap == null) {
+                return;
+            }
+            // 构造定位数据
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(location.getDirection())//获取方向
+                    .latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+
+            // 设置定位数据
+            mBaiduMap.setMyLocationData(locData);
+            BitmapDescriptor mCurrentMarker = BitmapDescriptorFactory
+                    .fromResource(R.drawable.mark);
+            MyLocationConfiguration config = new MyLocationConfiguration(mCurrentMode, true, mCurrentMarker);
+            mBaiduMap.setMyLocationConfigeration(config);
+            //
+
+            //Receive Location
+            StringBuffer sb = new StringBuffer(256);
+            sb.append("time : ");
+            sb.append(location.getTime());
+            sb.append("\nerror code : ");
+            sb.append(location.getLocType());
+            sb.append("\nlatitude : ");
+            sb.append(location.getLatitude());
+            sb.append("\nlontitude : ");
+            sb.append(location.getLongitude());
+            sb.append("\nradius : ");
+            sb.append(location.getRadius());
+            if (location.getLocType() == BDLocation.TypeGpsLocation) {// GPS定位结果
+                sb.append("\nspeed : ");
+                sb.append(location.getSpeed());// 单位：公里每小时
+                sb.append("\nsatellite : ");
+                sb.append(location.getSatelliteNumber());
+                sb.append("\nheight : ");
+                sb.append(location.getAltitude());// 单位：米
+                sb.append("\ndirection : ");
+                sb.append(location.getDirection());// 单位度
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                sb.append("\ndescribe : ");
+                sb.append("gps定位成功");
+
+            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {// 网络定位结果
+                sb.append("\naddr : ");
+                sb.append(location.getAddrStr());
+                //运营商信息
+                sb.append("\noperationers : ");
+                sb.append(location.getOperators());
+                sb.append("\ndescribe : ");
+                sb.append("网络定位成功");
+            } else if (location.getLocType() == BDLocation.TypeOffLineLocation) {// 离线定位结果
+                sb.append("\ndescribe : ");
+                sb.append("离线定位成功，离线定位结果也是有效的");
+            } else if (location.getLocType() == BDLocation.TypeServerError) {
+                sb.append("\ndescribe : ");
+                sb.append("服务端网络定位失败，可以反馈IMEI号和大体定位时间到loc-bugs@baidu.com，会有人追查原因");
+            } else if (location.getLocType() == BDLocation.TypeNetWorkException) {
+                sb.append("\ndescribe : ");
+                sb.append("网络不同导致定位失败，请检查网络是否通畅");
+            } else if (location.getLocType() == BDLocation.TypeCriteriaException) {
+                sb.append("\ndescribe : ");
+                sb.append("无法获取有效定位依据导致定位失败，一般是由于手机的原因，处于飞行模式下一般会造成这种结果，可以试着重启手机");
+            }
+            sb.append("\nlocationdescribe : ");
+            sb.append(location.getLocationDescribe());// 位置语义化信息
+            List<Poi> list = location.getPoiList();// POI数据
+            if (list != null) {
+                sb.append("\npoilist size = : ");
+                sb.append(list.size());
+                for (Poi p : list) {
+                    sb.append("\npoi= : ");
+                    sb.append(p.getId() + " " + p.getName() + " " + p.getRank());
+                }
+            }
+            Log.i("BaiduLocationApiDem", sb.toString());
+        }
     }
 
     @Override
@@ -67,16 +330,64 @@ public class MainActivity extends AppCompatActivity {
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
         mMapView.onPause();
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.yeqinglu.map/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.yeqinglu.map/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+
+        // 退出时销毁定位
+        mLocationClient.stop();
+        // 关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
     }
 }
